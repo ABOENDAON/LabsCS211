5f68f6e18441dd76a9edf9ee45cb962d2daadc6e
ABOENDAON <nathantruongnguyen33@proton.me>
2025-02-10 21:26:36 -0800

    lab 3 completed

--- Lab3/library.cpp
+++ Lab3/library.cpp
@@ -1,21 +1,77 @@
 #include "library.h"
+#include <iostream>
 #include <string>
-
+#include <cctype>
 using namespace std;
 
 
 bool valid_identifier(const string& str)
+
 {
 
+
+    // Check if string is empty
+    if(str.empty()) 
+    {
+        return true;
+        
+    }
+    // Check if first character is a number
+    if(isdigit(str[0]))
+    {
+        return false;
+    }
+    // Loop through the string and finds if the character is a letter, digit, or underscore
+    for (char ch : str) 
+    {
+    if (!(isalnum(ch) || ch == '_')) 
+        {
+        return false;
+        }
+    }
+    return true;
 }
+
 
 string shortened_date(const string& full_date)
+
 {
+    size_t first_comma = full_date.find(',');
+    string shorten_weekday = full_date.substr(0, 3);  // Extract first 3 letters of weekday
+
+    // Find the position of the second comma after weekday
+
+    size_t second_comma = full_date.find(',', first_comma + 1);
+
+
+    // Extract the first three letters of the month
+
+    size_t month_start = first_comma + 2;  // Skip the comma and space
+
+    string shorten_month = full_date.substr(month_start, 3); // First 3 letters of month
+
+    // Find year
+    string year = full_date.substr(second_comma + 2); // Skip the comma and space, extract year
+
+
+    // Return the formatted shortened date
+
+    return shorten_weekday + ", " + shorten_month + ", " + year;
 
 }
 
-void find_replace(string& text, const string& find_str, const string&
-    replace_str)
+
+
+void find_replace(string& text, const string& find_str, const string& replace_str)
+
 {
-    
+    size_t pos = text.find(find_str);
+
+    while (pos != string ::npos ) 
+    {
+        text.replace(pos, find_str.length(), replace_str);
+
+        pos = text.find(find_str, pos + replace_str.length());
+    }
+
 }
--- Lab3/string.cpp
+++ Lab3/string.cpp
@@ -1,12 +1,69 @@
-#include "library.cpp"
+#include "library.h"
 #include <iostream>
 #include <string>
-
+#include <cctype>
+#include <iomanip>
 using namespace std;
 
 int main() 
 {
- string sentence;
- cout << "Enter a string: " << endl;
- cin >> sentence; 
+    // PART 1:
+
+    // Makes reading boolean 0 and 1 readable to people
+    cout << boolalpha; 
+    cout << "\n" << endl;
+    cout << "\nPART 1: \n" << endl;
+    cout << "IDENTIFIERS" << setw(15) <<  "VALID OR NOT" << endl;
+    cout << "ab_CD_1" << setw(15) << valid_identifier("ab_CD_1") << endl; // true
+    cout << "abCD1" << setw(17) << valid_identifier("abCD1") << endl;   // true
+    cout << "_" << setw(21) << valid_identifier("_") << endl;        // true
+    cout << "a" << setw(21) << valid_identifier("a") << endl;        // true
+    cout << "_1" << setw(20) << valid_identifier("_1") << endl;       // true
+    cout << "C_" << setw(20) << valid_identifier("C_") << endl;       // true
+    cout << "ab-CD-1" <<setw(16) <<  valid_identifier("ab-CD-1") << endl;  // false
+    cout << "ab CD 1" << setw(16) << valid_identifier("ab CD 1") << endl;  // false
+    cout << "1a" << setw(21) <<valid_identifier("1a") << endl;       // false
+    cout << "a$" << setw(21) << valid_identifier("a$") << endl;       // false
+    cout << "\n" << endl;
+
+    // PART 2:
+    
+    cout << "\nPART 2: \n" << endl;
+    cout <<  setw(0) << "PARAMETER" << setw(35) << "RETURNED VALUE" << endl;
+    cout << "Tuesday, February 8, 2024" << setw(19) << shortened_date("Tuesday, February 8, 2024") << endl;  
+    cout << "Thursday, Apirl 1, 2022" <<  setw(21) << shortened_date("Thursday, Apirl 1, 2022") << endl;     
+    cout << "Sunday, May 31, 2023" <<  setw(24) << shortened_date("Sunday, May 31, 2023") << endl; 
+    cout << "\n" << endl;
+    
+    // Part 3:
+
+    cout << "\nPART 3: \n" << endl;
+    
+    cout << "text BEFORE" << setw(20) << "find_str" << setw(20) << "replace_str" << setw(20) << "text AFTER" << endl;
+
+    string text1 = "ababaab";
+    string one = text1;
+    find_replace(text1, "ab", "cd");
+    cout << one << setw(21) << "ab" << setw(18) << "cd" << setw(24) << text1 << endl;
+
+    string text2 = "cdcdacd";
+    string two = text1;
+    find_replace(text1, "da", "cc");
+    cout << two << setw(21) << "da" << setw(18) << "cc" << setw(24) << text1 << endl;
+
+    string text3 = "cdccccd";
+    string three = text1;
+    find_replace(text1, "cc", "ab");
+    cout << three << setw(21) << "cc" << setw(18) << "ab" << setw(24) << text1 << endl;
+
+    string text4 = "cdababd";
+    string four = text1;
+    find_replace(text1, "aa", "gc");
+    cout << four << setw(21) << "aa" << setw(18) << "gc" << setw(24) << text1 << endl;
+
+    cout << "\n" << endl;
+
+
+    
+    return 0;
 }
